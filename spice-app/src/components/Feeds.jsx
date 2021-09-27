b47e988f401ba9ade37ed1af1d2f887ce3151c16
Emmanuel Iyere <40471024+emmanuelky@users.noreply.github.com>
2021-09-27 16:13:41 +0200

    created db schema

--- spice-app/src/components/Feeds.jsx
+++ spice-app/src/components/Feeds.jsx
@@ -10,14 +10,15 @@ const Feeds = () => {
     const ID = uuidv4()
 
     useEffect(() => {
-        axios.post(`${process.env.REACT_APP_BASE_URL}/posts/`, {
-            postid: ID,
-            postmessage: "wondegdtdgrful",
-            postimage: [
-                "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
-            ],
-            likes: []
-        })
+        axios.post(`${process.env.REACT_APP_BASE_URL}/posts/`,
+            {
+                postid: ID,
+                postmessage: "wondegdtdgrful",
+                postimage: [
+                    "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
+                ],
+                likes: []
+            })
             .then(function (response) {
                 console.log(response);
             })
--- spice-app/src/components/HomePage.jsx
+++ spice-app/src/components/HomePage.jsx
@@ -1,8 +1,31 @@
+// import axios from 'axios'
+// import { v4 as uuidv4 } from 'uuid';
 import React, { useEffect, useState } from 'react'
 
 
 const HomePage = () => {
+    // const ID = uuidv4()
 
+    // const data =
+    // {
+    //     "createdAt": "2020-12-01T21:48:26.995Z",
+    //     "text": "wonderful",
+    //     "updatedAt": "2020-12-01T21:50:50.616Z",
+    //     "user": {},
+    //     "username": "meC",
+    //     "id": ID,
+    // }
+
+
+    // useEffect(() => {
+    //     axios.post(`${process.env.REACT_APP_BASE_URL}/posts/`, data)
+    //         .then(function (response) {
+    //             console.log(response);
+    //         })
+    //         .catch(function (error) {
+    //             console.log(error);
+    //         });
+    // }, [])
 
 
     return (
