1339aa2858819d8e9750d57f27a8d7c63577e688
Emmanuel Iyere <40471024+emmanuelky@users.noreply.github.com>
2021-10-19 22:15:13 +0200

    fixing deployment

--- src/App.js
+++ src/App.js
@@ -19,18 +19,20 @@ function App() {
 
 
   return (
-    <Router basename='/'>
+    <Router basename='/React'>
 
       <div className=" bg-gray-900 min-h-screen ">
-        <Route path="/" exact component={HomePage} />
-        <Route path="/login" exact component={Login} />
-        <Route path="/signup" exact component={SignUp} />
-        <Route path="/music" exact component={Music} />
-        <Route path="/crypto" exact component={CryptoHomePage} />
-        <Route path="/movies" exact component={Movies} />
-        <Route path="/profile/:id" exact component={ProfilePage} />
-        <Route path="/movie/:id/credits" exact component={MovieDetails} />
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/signup" exact component={SignUp} />
+          <Route path="/music" exact component={Music} />
+          <Route path="/crypto" exact component={CryptoHomePage} />
+          <Route path="/movies" exact component={Movies} />
+          <Route path="/profile/:id" exact component={ProfilePage} />
+          <Route path="/movie/:id/credits" exact component={MovieDetails} />
 
+        </Switch>
 
 
 
